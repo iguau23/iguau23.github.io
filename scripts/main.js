9a00fcf65a736154dc03288850f5ce979c7b21c6
iguau23 <leonardotkimura@gmail.com>
2018-06-05 15:33:36 -0300

    video youtube

--- scripts/main.js
+++ scripts/main.js
@@ -21,6 +21,27 @@ function commandPressed(command){
   myCommand = command;
 }
 
+function videoPressed(command){
+  var video = document.getElementById("myVideo");
+  switch (command) {
+    case 'demo':
+      video.src = "https://www.youtube.com/embed/5NhLg1F9pMo?controls=1&autoplay=1&rel=0&showinfo=0&modestbranding=0";
+      break;
+    case 'espiral':
+      video.src = "https://www.youtube.com/embed/74nm7vb4HPI?autoplay=1&controls=1&rel=0&showinfo=0&modestbranding=0";
+      break;
+    case 'esquadrilha':
+      video.src = "https://www.youtube.com/embed/dWoIljGEOQ4?autoplay=1&controls=1&rel=0&showinfo=0&modestbranding=0";
+      break;
+    default:
+      video.src = "https://www.youtube.com/embed/5NhLg1F9pMo?autoplay=0&controls=0&rel=0&showinfo=0&modestbranding=0";
+      break;
+
+  }
+
+}
+
+
 function getStatus(){
   var xmlhttp = new XMLHttpRequest();
   var urlPeekStatus = "https://3vkeycenej.execute-api.us-east-1.amazonaws.com/prod/CIAB-2018-DroneCommandQueue?droneId=status&operation=PEEK&secret=dr0neRulez2A5T7U"
@@ -36,5 +57,11 @@ function getStatus(){
   setTimeout(getStatus, 1000);
 
 }
+function setVideoRate(){
+   var video = document.getElementById("myVideo");
+  var width = video.clientHeight*(16/9);
+  video.style.width = width;
+}
 
 getStatus()
+setVideoRate()
